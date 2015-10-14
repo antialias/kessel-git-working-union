8be6aca2dc91d9ad190da5388ca85dfda7eb3d27
Thomas Hallock <thomas@1stdibs.com>
2015-10-14 16:04:03 -0400

    handling case where cwd is not the git root

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var path = require('path');
 var invert = require('lodash.invert');
+var assign = require('lodash.assign');
 var execSync = require('execSync'); // TODO: use child_process.execSync once we get to node 4
 module.exports = function (options) {
     this._options = options || {};
@@ -6,13 +8,19 @@ module.exports = function (options) {
 module.exports.prototype = {
     apply: function (kesselrun) {
         kesselrun.plugin('reduce-unglobbed-jasmine-specs', function (specFiles) {
-            var specFileMap = invert(specFiles);
-            return execSync.exec('git diff --name-only', {
-                cwd: this._options.cwd || process.cwd(),
-                encoding: 'utf8'
-            }).stdout.trim().split("\n").filter(function (newFile) {
-                return !!specFileMap[newFile];
-            });
+            var pathFromRoot = this._pathFromGitRoot();
+            var oldCwd = process.cwd();
+            var dirtySpecFiles;
+            process.chdir(this._options.cwd || process.cwd());
+            var dirtyFileMap = invert(execSync.exec('git diff --name-only').stdout.trim().split("\n"));
+            dirtySpecFiles = specFiles.filter(function (specFile) {
+                return !!dirtyFileMap[path.join(pathFromRoot, kesselrun.jasmineConfig.spec_dir, specFile)];
+            }.bind(this));
+            process.chdir(oldCwd);
+            return dirtySpecFiles;
         }.bind(this));
+    },
+    _pathFromGitRoot: function () {
+        return path.relative(execSync.exec('git rev-parse --show-toplevel').stdout.trim(), process.cwd());
     }
 };
--- test/index.js
+++ test/index.js
@@ -6,10 +6,14 @@ var WorkingUnion = require('../');
 var assert = require('assert');
 describe('kessel git working union', function () {
     it('should union the set of specfiles with the set of unstaged files in gits working directory', sinon.test(function () {
+        this.stub(WorkingUnion.prototype, '_pathFromGitRoot', function () {return ''});
         this.stub(execSync, 'exec', function () {
             return {stdout: ['fizz', 'foo', 'bar'].join("\n")};
         });
         new WorkingUnion().apply({
+            jasmineConfig: {
+                spec_dir: ''
+            },
             plugin: function (hook, cb) {
                 assert.equal(hook, 'reduce-unglobbed-jasmine-specs');
                 assert.deepEqual(
