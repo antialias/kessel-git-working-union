c6fd091d08a4ebe27549f5e8948ad22b70b1d09d
Thomas Hallock <thomas@1stdibs.com>
2015-11-23 12:21:42 -0500

    calling it childProces instead of execSync

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-var execSync = require('child_process');
+var childProcess = require('child_process');
 var sinon = require('sinon');
 var WorkingUnion = require('../');
 var assert = require('assert');
 describe('kessel git working union', function () {
     it('should union the set of specfiles with the set of unstaged files in gits working directory', sinon.test(function () {
         this.stub(WorkingUnion.prototype, '_pathFromGitRoot', function () {return ''});
-        this.stub(execSync, 'execSync', function () {
+        this.stub(childProcess, 'execSync', function () {
             return ['fizz', 'foo', 'bar'].join("\n");
         });
         new WorkingUnion().apply({
